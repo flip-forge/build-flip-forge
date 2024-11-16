2a3dcabb55398aea3aa2e3e221f6fd8a31178c72
alexkiro <1538458+alexkiro@users.noreply.github.com>
2024-11-16 14:49:12 +0200

    Fix loading inputs and env

--- build/action.ts
+++ build/action.ts
@@ -2,7 +2,10 @@ import path from "node:path";
 import fs from "node:fs";
 import console from "node:console";
 import { execFileSync, type ExecFileSyncOptions } from "node:child_process";
-import core from "@actions/core";
+
+const STEPS = JSON.parse(process.env.STEPS ?? "{}");
+const INPUTS = JSON.parse(process.env.INPUTS ?? "{}");
+const GITHUB = JSON.parse(process.env.GITHUB ?? "{}");
 
 function ensureTrailingSlash(str: string): string {
   return str.endsWith("/") ? str : `${str}/`;
@@ -77,20 +80,49 @@ function convertPagetoJPG(pdfPath: string, page: number) {
   ]);
 }
 
-function main(): void {
-  const pdfPath = path.join(
-    process.env.GITHUB_WORKSPACE ?? "",
-    core.getInput("file", { required: true, trimWhitespace: true }) ?? "",
+function buildSite(pdfInfo: Record<string, string>) {
+  const [imgWidth, imgHeight] = getImageSize("public/page-1.jpg");
+  const [owner, repo] = (GITHUB.repository ?? "/").split("/");
+  const baseUrl = ensureTrailingSlash(
+    STEPS.configure?.outputs?.base_path || `${repo}/`,
   );
+  const fullUrl = ensureTrailingSlash(
+    STEPS.configure?.outputs?.base_url || `https://${owner}.github.io/${repo}/`,
+  );
+
+  const env = {
+    ...process.env,
+    VITE_BASE_URL: baseUrl,
+    VITE_FULL_URL: fullUrl,
+    VITE_TITLE: INPUTS.title || pdfInfo.title || "Flipbook",
+    VITE_BACKGROUND_COLOR: INPUTS.backgroundcolor ?? "#000000",
+    VITE_TOOLBAR_COLOR: INPUTS.backgroundcolor ?? "#ffffff",
+    VITE_DESCRIPTION: INPUTS.description ?? "",
+    VITE_PAGE_NUMBER: pdfInfo.pages,
+    VITE_FILE_DOWNLOAD: path.basename(INPUTS.file),
+    VITE_SEO_IMAGE: "page-1.jpg",
+    VITE_SEO_IMAGE_URL: `${fullUrl}page-1.jpg`,
+    VITE_SEO_IMAGE_WIDTH: String(imgWidth),
+    VITE_SEO_IMAGE_HEIGHT: String(imgHeight),
+  };
+
+  execCommand(["npm", "run", "build-only"], { env });
+}
+
+function main(): void {
+  const pdfPath = path.join(GITHUB.workspace ?? "", INPUTS.file);
   if (!pdfPath || !fs.existsSync(pdfPath)) {
-    core.setFailed(`Could not find file: '${pdfPath}'`);
+    console.error("Could not find file:", pdfPath);
     process.exit(1);
   }
   const pdfInfo = getPdfInfo(pdfPath);
   const nrPages = parseInt(pdfInfo.pages ?? "0", 10);
 
   if (nrPages <= 0) {
-    core.setFailed("Unable to parse book info, could not find number of pages");
+    console.error(
+      "Unable to parse book info, could not find number of pages",
+      pdfInfo,
+    );
     process.exit(1);
   }
 
@@ -104,33 +136,7 @@ function main(): void {
     convertPagetoJPG(pdfPath, page);
   }
 
-  const [owner, repo] = (process.env.GITHUB_REPOSITORY ?? "/").split("/");
-  const fullUrl = ensureTrailingSlash(
-    process.env.VITE_FULL_URL || `https://${owner}.github.io/${repo}/`,
-  );
-  const [imgWidth, imgHeight] = getImageSize("public/page-1.jpg");
-
-  const env = {
-    ...process.env,
-    VITE_BASE_URL: ensureTrailingSlash(process.env.VITE_BASE_URL || `${repo}/`),
-    VITE_FULL_URL: fullUrl,
-    VITE_TITLE:
-      core.getInput("title", { trimWhitespace: true }) ||
-      pdfInfo.title ||
-      "Flipbook",
-    VITE_BACKGROUND_COLOR: core.getInput("backgroundColor") ?? "#000000",
-    VITE_TOOLBAR_COLOR: core.getInput("backgroundColor") ?? "#ffffff",
-    VITE_DESCRIPTION: core.getInput("description") ?? "",
-    VITE_PAGE_NUMBER: String(nrPages),
-    VITE_FILE_DOWNLOAD: path.basename(pdfPath),
-    VITE_SEO_IMAGE: "page-1.jpg",
-    VITE_SEO_IMAGE_URL: `${fullUrl}page-1.jpg`,
-    VITE_SEO_IMAGE_WIDTH: String(imgWidth),
-    VITE_SEO_IMAGE_HEIGHT: String(imgHeight),
-  };
-
-  execCommand(["npm", "run", "build-only"], { env });
-  core.setOutput("dist", path.resolve("dist"));
+  buildSite(pdfInfo);
 }
 
 main();
